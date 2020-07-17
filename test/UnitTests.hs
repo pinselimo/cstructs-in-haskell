308d1480d227d93993ffb78608bb19bccf711450
Simon Plakolb <s.plakolb@gmail.com>
2020-07-17 12:51:16 +0200

    Update cabal for tests

--- test/UnitTests.hs
+++ test/UnitTests.hs
@@ -1,13 +1,16 @@
-module UnitTests where
+module Main where
 
 import Test.Framework.Providers.API
 import Test.Framework.Providers.HUnit
+import Test.Framework.Runners.Console (defaultMain)
 import Test.HUnit
 
 import Foreign.C.Structs
 import Foreign.Storable
 import Foreign.C.Types
 
-tests = testGroup "sizeOfStruct2" [
+main = defaultMain $ [sizeOfStruct2]
+
+sizeOfStruct2 = testGroup "sizeOfStruct2" [
     testCase "DoubleChar" $ 16 @?= sizeOf $ Struct2 0.5 1 :: Struct2 CDouble CChar
 ]
