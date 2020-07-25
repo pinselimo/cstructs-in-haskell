f157a7e8e41db2ddebe7e5874cccdb005425bad4
Simon Plakolb <s.plakolb@gmail.com>
2020-07-25 13:17:29 +0200

    Refactor

--- src/Foreign/C/Structs/Templates.hs
+++ src/Foreign/C/Structs/Templates.hs
@@ -4,7 +4,6 @@ module Foreign.C.Structs.Templates
 where
 
 import Language.Haskell.TH
-import Data.List (zipWith4)
 
 import Foreign.Storable (Storable, peek, poke, sizeOf, alignment)
 import Foreign.Ptr (castPtr)
@@ -13,20 +12,31 @@ import Foreign.C.Structs.Utils (next, sizeof, fmax)
 structT :: Int -> DecsQ
 structT = return . zipWith ($) [structTypeT, storableInstanceT] . repeat
 
-sTypeN n = mkName ("Struct" ++ show n)
-struct   = mkName "struct"
-ptr      = mkName "ptr"
+-- Templating functions
 
-fieldnames :: String -> [Name]
-fieldnames s = map (mkName . (:s)) ['a'..'z']
-getters    :: Int -> [Name]
-getters n = map (mkName . (("s" ++ show n) ++))
-          $  ["fst","snd","trd","fth","fif","sxt","sth","egt","nth"]
-          ++ [show n ++ "th" | n <- [10..]]
+structTypeT :: Int -> Dec
+structTypeT nfields = DataD [] (sTypeN nfields) tyVars Nothing [constructor] [deriv]
+    where tyVars    = map PlainTV $ take nfields $ fieldnames ""
+          constructor = RecC (sTypeN nfields) $ take nfields $ records
+          records     = zipWith defRec (getters nfields) (fieldnames "")
+          defRec n t  = (,,) n (Bang NoSourceUnpackedness NoSourceStrictness) (VarT t)
+          deriv = DerivClause Nothing [ConT ''Show, ConT ''Eq]
 
-vals f n s = take n $ zipWith val (fieldnames s) (getters n)
-    where val v getter = ValD (VarP v) (NormalB $ body getter) []
-          body getter  = AppE (VarE f) $ AppE (VarE getter) $ VarE struct
+storableInstanceT :: Int -> Dec
+storableInstanceT nfields = InstanceD Nothing cxt tp decs
+    where vars = take nfields $ fieldnames ""
+          storable = AppT $ ConT ''Storable
+
+          cxt  = map (storable . VarT) vars
+          tp   = storable $ foldl AppT (ConT $ sTypeN nfields) $ map VarT vars
+
+          decs = [ sizeOfT nfields
+                 , alignmentT nfields
+                 , peekT nfields
+                 , pokeT nfields
+                 ]
+
+-- Storable instance function temaples
 
 sizeOfT :: Int -> Dec
 sizeOfT nfields = FunD 'sizeOf [clause]
@@ -50,15 +60,13 @@ peekT nfields = FunD 'peek [clause]
           clause = Clause [VarP ptr] (NormalB body) []
 
           body = DoE $ initial ++ concat gotos ++ final
-          initial = [ BindS (VarP $ head vars) (AppE (VarE 'peek) (AppE (VarE 'Foreign.Ptr.castPtr) (VarE ptr)))
+          initial = [ BindS (VarP $ head vars) (AppE (VarE 'peek) cast_ptr)
                     , BindS (VarP $ head ptrs) (AppE (AppE (VarE 'next) $ VarE ptr) $ VarE $ head vars)
                   ]
           gotos = zipWith3 goto (tail vars) (ptrs) (tail ptrs)
-          goto n p next_p = [bind_var, bind_ptr]
-                where bind_var = BindS (VarP n) (AppE (VarE 'peek) $ VarE p)
-                      bind_ptr = BindS (VarP next_p) (AppE (AppE (VarE 'next) $ VarE p) $ VarE n)
+          goto n p next_p = [bind_var p n, bind_ptr next_p p (VarE n)]
 
-          final = [ BindS (VarP $ last vars) (AppE (VarE 'peek) $ VarE $ last ptrs)
+          final = [ bind_var (last ptrs) (last vars)
                   , NoBindS $ AppE (VarE 'return) $ foldl AppE (ConE (sTypeN nfields)) (map VarE vars)
                 ]
 
@@ -71,35 +79,40 @@ pokeT nfields = FunD 'poke [clause]
 
           patterns = [VarP ptr, ConP (sTypeN nfields) (map VarP vars)]
           body = DoE $ [init_poke, init_next] ++ concat gotos ++ [final]
-          init_poke = NoBindS $ AppE (AppE (VarE 'poke) $ AppE (VarE 'Foreign.Ptr.castPtr) $ VarE ptr) $ VarE $ head vars
-          init_next = BindS (VarP $ head ptrs) (AppE (AppE (VarE 'next) $ VarE ptr) $ VarE $ head vars)
-          final = NoBindS $ AppE (AppE (VarE 'poke) $ VarE $ last ptrs) $ VarE $ last vars
+
+          init_poke = NoBindS
+                    $ AppE cast_poke_ptr (VarE $ head vars)
+                    where  cast_poke_ptr = AppE (VarE 'poke) cast_ptr
+          init_next = bind_ptr (head ptrs) ptr (VarE $ head vars)
 
           gotos = zipWith3 goto (tail vars) (ptrs) $ tail ptrs
-          goto n p next_p = [poke_var, bind_ptr]
+          goto n p next_p = [poke_var p var, bind_ptr next_p p var]
                 where var = VarE n
-                      poke_var = NoBindS $ AppE (AppE (VarE 'poke) $ VarE p) var
-                      bind_ptr = BindS (VarP next_p) $ AppE (AppE (VarE 'next) $ VarE p) var
 
-structTypeT :: Int -> Dec
-structTypeT nfields = DataD [] (sTypeN nfields) tyVars Nothing [constructor] [deriv]
-    where tyVars    = map PlainTV $ take nfields $ fieldnames ""
-          constructor = RecC (sTypeN nfields) $ take nfields $ records
-          records     = zipWith defRec (getters nfields) (fieldnames "")
-          defRec n t  = (,,) n (Bang NoSourceUnpackedness NoSourceStrictness) (VarT t)
-          deriv = DerivClause Nothing [ConT ''Show, ConT ''Eq]
+          final = poke_var (last ptrs) (VarE $ last vars)
 
-storableInstanceT :: Int -> Dec
-storableInstanceT nfields = InstanceD Nothing cxt tp decs
-    where vars = take nfields $ fieldnames ""
-          storable = AppT $ ConT ''Storable
+-- Helpers and Constants
 
-          cxt  = map (storable . VarT) vars
-          tp   = storable $ foldl AppT (ConT $ sTypeN nfields) $ map VarT vars
+sTypeN n = mkName ("Struct" ++ show n)
+struct   = mkName "struct"
+ptr      = mkName "ptr"
+cast_ptr = AppE (VarE 'castPtr) (VarE ptr)
 
-          decs = [ sizeOfT nfields
-                 , alignmentT nfields
-                 , peekT nfields
-                 , pokeT nfields
-                 ]
+fieldnames :: String -> [Name]
+fieldnames s = map (mkName . (:s)) ['a'..'z']
+getters    :: Int -> [Name]
+getters n = map (mkName . (("s" ++ show n) ++))
+          $  ["fst","snd","trd","fth","fif","sxt","sth","egt","nth"]
+          ++ [show n ++ "th" | n <- [10..]]
+
+vals f n s = take n $ zipWith val (fieldnames s) (getters n)
+    where val v getter = ValD (VarP v) (NormalB $ body getter) []
+          body getter  = AppE (VarE f) $ AppE (VarE getter) $ VarE struct
+
+bind_var ptr var = BindS (VarP var) (AppE (VarE 'peek) $ VarE ptr)
+poke_var ptr var = NoBindS
+       $ AppE (AppE (VarE 'poke) $ VarE ptr) var
+bind_ptr np pp var = BindS (VarP np)
+       $ AppE next_ptr var
+       where next_ptr = AppE (VarE 'next) $ VarE pp
 
