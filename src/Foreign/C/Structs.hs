b93673b7682e92606229abb9e4382436beeb9fba
Simon Plakolb <s.plakolb@gmail.com>
2020-08-08 12:17:17 +0200

    Change website to refer to readme

--- src/Foreign/C/Structs.hs
+++ src/Foreign/C/Structs.hs
@@ -7,6 +7,30 @@ Maintainer      : s.plakolb@gmail.com
 Stability       : beta
 
 The @Foreign.C.Structs@ module allows you to construct C structs of arbitrary @Storable@ types.
+It also defined them as instances of the Storable type-class. You can thus create pointers
+to an instance of such a struct and interface with another language.
+
+Currently up to six records are supported. Each number of records needs its own type.
+The types are named after the number of records they support: 'Struct2', 'Struct3' .. @StructN@
+
+If a Struct type with more fields is required, it can be created using Template Haskell and the 'structT' function:
+
+> structT 8 -- creates a Struct with 8 fields
+
+Field access is provided threefold:
+
+ * Record syntax
+
+> 2nd :: Struct2 a b -> b
+
+ * Pattern matching
+
+> (Struct2 a b)
+
+ * Template Haskell 'acs' function.
+
+> $(acs 2 2) :: Struct2 a b -> b
+
 -}
 module Foreign.C.Structs (
       Struct2(..)
@@ -46,24 +70,4 @@ import Foreign.C.Structs.Utils (
     ,fmax
     )
 
-{- |
-C-Structs
----------
-
-The @Foreign.C.Structs@ module allows you to construct C structs of arbitrary @Storable@ types.
-It also defined them as instances of the Storable type-class. You can thus create pointers
-to an instance of such a struct and interface with another language.
-
-Currently up to six records are supported. Each number of records needs its own type.
-The types are named after the number of records they support: 'Struct2', 'Struct3' .. @StructN@
-
-If a Struct type with more fields is required, it can be created using Template Haskell and the 'structT' function:
-
-> structT 8 -- creates a Struct with 8 fields
-
-Field access is provided threefold:
- * Record syntax
- * Pattern matching
- * Template Haskell 'acs' function.
--}
 
